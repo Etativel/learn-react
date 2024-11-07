cbdb65a9f5aa8082cba8b5e2e9a26be1746b230c
Etativel <farhanmaulana4611@gmail.com>
2024-11-07 13:25:36 +0800

    feat: add counter for color change

--- src/exercise/BackgroundChanger.jsx
+++ src/exercise/BackgroundChanger.jsx
@@ -4,8 +4,15 @@ const COLORS = ["pink", "green", "blue", "yellow", "gray", "purple", "brown"];
 
 function BackgroundChanger() {
   const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
+  const [count, setCount] = useState(0);
   function handleChangeColor(color) {
     setBackgroundColor(color);
+  }
+  function handleCounter(color) {
+    if (color === backgroundColor) {
+      return;
+    }
+    setCount(count + 1);
   }
   return (
     <div
@@ -14,15 +21,23 @@ function BackgroundChanger() {
         backgroundColor,
       }}
     >
-      {COLORS.map((color) => (
-        <button
-          className={`colorBtn ${backgroundColor === color ? "selected" : ""}`}
-          key={color}
-          onClick={() => handleChangeColor(color)}
-        >
-          {color}
-        </button>
-      ))}
+      <h2>Color has been change {count} times</h2>
+      <div className="buttonContainer">
+        {COLORS.map((color) => (
+          <button
+            className={`colorBtn ${
+              backgroundColor === color ? "selected" : ""
+            }`}
+            key={color}
+            onClick={() => {
+              handleChangeColor(color);
+              handleCounter(color);
+            }}
+          >
+            {color}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
