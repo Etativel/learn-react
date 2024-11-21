eea58cec2bfdc1351ab1ea4de88078f2090f9306
Etativel <farhanmaulana4611@gmail.com>
2024-11-21 09:55:18 +0800

    feat: add react testing

--- src/exercise/Ppp.jsx
+++ src/exercise/Ppp.jsx
@@ -1,7 +1,16 @@
-export default function Ppp({ title }) {
+import { useState } from "react";
+
+export default function Ppp() {
+  const [title, setTitle] = useState("react");
+
+  function handleClick() {
+    setTitle("yellow");
+  }
+
   return (
     <div className="headline">
       <h1>{title}</h1>
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 }
--- src/exercise/__tests__/Ppp.test.jsx
+++ src/exercise/__tests__/Ppp.test.jsx
@@ -1,13 +1,31 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
+import userEvent from "@testing-library/user-event";
 import Ppp from "../Ppp";
 
 describe("App", () => {
   it("renders headline", () => {
-    render(<Ppp title="React" />);
+    render(<Ppp />);
 
     screen.debug();
 
-    expect(screen.getByRole("heading").textContent).toBe("React");
+    expect(screen.getByRole("heading").textContent).toBe("react");
+  });
+
+  it("Ppp componenet", () => {
+    const { container } = render(<Ppp />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it("user click", async () => {
+    render(<Ppp />);
+
+    // const button = screen.getByRole("button", { name: "Click me" });
+    const button = screen.getByText("click me", { exact: false });
+
+    await userEvent.click(button);
+
+    expect(screen.getByRole("heading").textContent).toMatch(/yellow/i);
   });
 });
